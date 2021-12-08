333663fcac2127fd85c42d4b017c05d6462a28be
Kungg <1592500694@qq.com>
2021-12-09 00:09:30 +0800

    fix: config配置404中间件

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -9,9 +9,13 @@ export default (appInfo: EggAppInfo) => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1611038425326_4049'
   // add your config here
-  config.middleware = ['errorMiddleware'],
+  config.middleware = ['errorMiddleware','notFoundMiddleware'],
   config.orm ={
-
+    type: 'mysql',
+    host: 'rm-bp1qqu1t135v4d9x67o.mysql.rds.aliyuncs.com',
+    port: 3306,
+    username: 'root',
+    password: 'Aarootroot1',
     database: 'socks',
     synchronize: true, // 如果第一次使用，不存在表，有同步的需求可以写 true
     logging: false,
